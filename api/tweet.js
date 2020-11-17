f8145414d884ea70520f99e0c1cd41072fed760e
jasonelliots <j.elliotschwartz@gmail.com>
2020-11-17 09:42:25 -0800

    cents per sat first pass

--- api/tweet.js
+++ api/tweet.js
@@ -15,13 +15,21 @@ const client = new Twitter({
 
 module.exports = async (req, res) => {
     const post = await axios.get(
-        "https://www.reddit.com/r/NatureIsFuckingLit/hot.json?limit=1"
+        // hit coingecko bitcoin price endpoint 
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
     );
-    const hey = await post.data.data.children[1].data.title;
-    const link = await post.data.data.children[1].data.url_overridden_by_dest; 
+
+    // grab relevant info 
+    const btcprice = await post.data.bitcoin.usd;
+
+    // do necessary operations to get to cents per sat 
+    
+    const centspersat = (btcprice/100000000)*100 
+
 
     await client
-        .post("statuses/update", { status: `${hey} ...wow! ${link}`})
+        .post("statuses/update", { status: `1 satoshi = ${centspersat} cents`})
         .then((result) => {
             console.log(result)
         })
@@ -30,3 +38,20 @@ module.exports = async (req, res) => {
     await res.send("Done");
 }
 
+
+// module.exports = async (req, res) => {
+//     const post = await axios.get(
+//         "https://www.reddit.com/r/NatureIsFuckingLit/hot.json?limit=1"
+//     );
+//     const hey = await post.data.data.children[1].data.title;
+//     const link = await post.data.data.children[1].data.url_overridden_by_dest; 
+
+//     await client
+//         .post("statuses/update", { status: `${hey} ...wow! ${link}`})
+//         .then((result) => {
+//             console.log(result)
+//         })
+//         .catch((error) => console.log(error));
+    
+//     await res.send("Done");
+// }
